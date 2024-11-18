a35282899250ba172d9610563f7a542d7d08a335
bart <bielski.bart1012@gmail.com>
2024-11-18 10:21:23 +0000

    Inital commit

---
+++ UNitTestingChallenge/UNitTestingChallenge/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UNitTestingChallenge
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
