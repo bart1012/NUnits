755e6798829eae0ee240a515bf5502cdde6ef076
bart <bielski.bart1012@gmail.com>
2024-11-18 11:00:19 +0000

    Add FluentAssertion package

--- UNitTestingChallenge/NUnitTestingChallenge.Tests/UnitTest1.cs
+++ UNitTestingChallenge/NUnitTestingChallenge.Tests/UnitTest1.cs
@@ -1,16 +1,23 @@
+using UNitTestingChallenge;
+
+
 namespace NUnitTestingChallenge.Tests
 {
     public class Tests
     {
+        Compass myCompass;
+
         [SetUp]
         public void Setup()
         {
+            myCompass = new Compass(Point.South);
         }
 
         [Test]
         public void Test1()
         {
-            Assert.Pass();
+            var rotateResult = myCompass.Rotate(Point.North, Direction.Right);
+            //Assert.That(rotateResult, Is.EqualTo(Point.East);
         }
     }
 }
